91d8f74b270d824d724ab38ad4f8eb02cab19411
RafaMarques <RafaelMarques241002@gmail.com>
2024-02-13 17:15:31 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//efeito do botão voltar ao Topo
+function topo() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+//Validação de Login
+
+function login() {
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario === "admin" && senha === "123456") {
+        window.location.href = "index.html";
+        logado = 1;
+    }
+    if (logado === 0) {
+        alert("Acesso negado. Usuário ou senha incorretos!");
+    }
+}
+
+//Ativar alert no botão cadastrar
+
+function cadastro(){
+    alert("Usuário cadastrado!");
+    window.location.href = "index.html";
+}
